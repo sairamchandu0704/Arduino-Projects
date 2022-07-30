398123f7f04db8cfce7412394e142ff4640e5be9
SAI RAM CHANDU DADALA <109935257+sairamchandu0704@users.noreply.github.com>
2022-07-30 15:49:45 +0530

    Add files via upload

---
+++ LED_blinking.ino
@@ -0,0 +1,49 @@
+int a = 2;
+int b = 3;
+int c = 4;
+int d = 5;
+int e = 6;
+int f = 7;
+
+void setup() {
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(a,HIGH);
+  delay(50);
+  digitalWrite(a,LOW);
+  delay(50);
+  
+  digitalWrite(b,HIGH);
+  delay(50);
+  digitalWrite(b,LOW);
+  delay(50);
+  
+  digitalWrite(c,HIGH);
+  delay(50);
+  digitalWrite(c,LOW);
+  delay(50);
+  
+  digitalWrite(d,HIGH);
+  delay(50);
+  digitalWrite(d,LOW);
+  delay(50);
+  
+  digitalWrite(e,HIGH);
+  delay(50);
+  digitalWrite(e,LOW);
+  delay(50);
+
+   digitalWrite(f,HIGH);
+  delay(50);
+  digitalWrite(f,LOW);
+  delay(50);
+  
+
+}
